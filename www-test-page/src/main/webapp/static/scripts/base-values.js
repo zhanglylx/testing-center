092c0488e36cd545259d6c0ade36817c197c96de
zhangly <zhanglylx@163.com>
2020-03-02 15:57:35 +0800

    加入导航栏加载和所有下的工具加载

---
+++ www-test-page/src/main/webapp/static/scripts/base-values.js
@@ -0,0 +1,15 @@
+document.write("<script src='static/scripts/js-util.js'></script>");
+document.write("<script src='static/scripts/jquery/jquery3-4-1.js'></script>");
+document.write("<script src='static/scripts/jquery/jquery.md5.js'></script>");
+document.write("<script src='static/scripts/jquery/jquery.cookie.js'></script>");
+document.write("<script src='static/scripts/ue/ueditor.config.js'></script>");
+document.write("<script src='static/scripts/ue/ueditor.all.js'></script>");
+document.write("<script src='static/scripts/ue/ueditor.parse.js'></script>");
+document.write("<script src='static/scripts/ue/lang/zh-cn/zh-cn.js'></script>");
+
+
+var path_toolBox_findAll="toolBox/findAll";
+var path_toolList_findAll="toolList/findAll";
+
+
+
---
+++ www-test-page/src/main/webapp/static/scripts/tool.js
@@ -0,0 +1,14 @@
+function createToolDiv(data) {
+    var tool = '<div class="tools_div"><div class="tool_div">' +
+        '<img src="static/image/logo.png" >' +
+        '<div class="tool_name"><h3>' + data.tool_list_name + '</h3>' +
+        '<div class="tool_name_navigation" style="color: ' + data.tool_box_colour + '">[' + data.tool_box_name + ']</div>' +
+        '</div>' +
+        '<div class="clear_float"></div>' +
+        '<div class="too_halving_div"></div>' +
+        '<div class="tool_desc">' + data.tool_list_des + '</div>' +
+        '</div></div>';
+    var $tool = $(tool);
+    $tool.data("data", data);
+    return $tool;
+}
