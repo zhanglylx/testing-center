d9b863fec20cc59ee7433eca64947033cf32a474
zhangly <zhanglylx@163.com>
2020-03-30 17:14:30 +0800

    加入静态资源 测试机统计

--- www-test-dao/src/main/java/com/testing/center/cxb/ad/impl/GetAdDaoMapperImpl.java
+++ www-test-dao/src/main/java/com/testing/center/cxb/ad/impl/GetAdDaoMapperImpl.java
@@ -1,11 +1,12 @@
 package com.testing.center.cxb.ad.impl;
 
-import java.util.Map;
-import java.util.List;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+import java.util.*;
 
 import java.util.Map;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,6 +14,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.testing.center.cmmon.utils.ZLYStream.BuffReaderCalls;
+import com.testing.center.cmmon.utils.ZLYStream.StreamUtil;
 import com.testing.center.cmmon.utils.cxb.URLEnvironment;
 import com.testing.center.cmmon.utils.http.HttpUtils;
 import com.testing.center.cmmon.utils.http.NetworkHeaders;
@@ -23,6 +26,7 @@ import org.apache.http.message.BasicNameValuePair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StreamUtils;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -103,5 +107,67 @@ public class GetAdDaoMapperImpl implements GetAdDaoMapper {
         }
     }
 
+    public static void main(String[] args) throws IOException {
+        String url = "http://localhost:8080/test_www_page_war/ad/adLuaFunnel";
+        Map<String, Object> map = new HashMap<>();
+        map.put("uid", -1);
+        map.put("environment", 1);
+        map.put("adGG", "GG-31");
+        map.put("net", "wifi");
+        String response;
+//        PrintWriter printWriter = new PrintWriter(
+//                new OutputStreamWriter(
+//                        new FileOutputStream(new File("ad.txt")), StandardCharsets.UTF_8), true);
+        BufferedReader bufferedReader = StreamUtil.getCharacterBufferedReader(new File("ad.txt"));
+        String buffread;
+        int index = 0;
+        for (int i = 1000; i < 15000; i++) {
+            map.put("cnid", i);
+            if (i < 1800 && i > 1600) {
+                map.put("version", "5.7.2");
+                map.putAll(getApp(2));
+            } else if (i < 10100 && i > 10000) {
+                map.put("version", "5.7.3");
+                map.putAll(getApp(1));
+            } else if (i > 14000 && i < 14100) {
+                map.put("version", "5.7.0");
+                map.putAll(getApp(0));
+            } else {
+                continue;
+            }
+            response = HttpUtils.doPost(url, map);
+            buffread = bufferedReader.readLine();
+            if (!buffread.equals(response)) {
+                System.out.println(++index + " 检查失败");
+                System.out.println("预期结果：" + buffread);
+                System.out.println("实际结果：" + response);
+                return;
+            }
+//            printWriter.println(response);
+
+        }
+        System.out.println("检查通过");
+    }
+
+
+    public static Map<String, String> getApp(int j) {
+        Map<String, String> map = new HashMap<>();
+        switch (j) {
+            case 0:
+                map.put("appname", "aks");
+                map.put("packname", "com.mfyueduqi.book");
+                return map;
+            case 1:
+                map.put("appname", "mfzs");
+                map.put("packname", "com.mianfeizs.book");
+                return map;
+            case 2:
+                map.put("appname", "cxb");
+                map.put("packname", "com.mianfeia.book");
+                return map;
+            default:
+                throw new IllegalArgumentException(j + "");
+        }
+    }
 
 }
