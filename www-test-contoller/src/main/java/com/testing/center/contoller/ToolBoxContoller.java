69279877e0b7787b2eb66eddcefbf61d226f2202
zhangly <zhanglylx@163.com>
2020-04-06 17:24:27 +0800

    将@Controller更改为@RestController   这样此类下的接口返回JSON时不需要使用@ResponseBody

--- www-test-contoller/src/main/java/com/testing/center/contoller/ToolBoxContoller.java
+++ www-test-contoller/src/main/java/com/testing/center/contoller/ToolBoxContoller.java
@@ -1,28 +1,29 @@
 package com.testing.center.contoller;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.testing.center.cmmon.utils.TestingCenterResult;
 import com.testing.center.entity.ToolBox;
 import com.testing.center.service.ToolBoxService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.codec.cbor.Jackson2CborDecoder;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/toolBox")
+@Validated
 public class ToolBoxContoller {
     @Resource(name = "toolBoxService")
     private ToolBoxService toolBoxService;
 
     @RequestMapping("/findAll")
-    @ResponseBody
     public TestingCenterResult<List<ToolBox>> findAllToolBox() {
         return toolBoxService.findAll();
     }
