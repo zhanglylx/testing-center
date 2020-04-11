6862a81902af357b8e76793c4de75f0373c327d6
zhangly <zhanglylx@163.com>
2020-04-11 18:33:30 +0800

    添加lombok插件

--- www-test-dao/src/main/java/com/testing/center/entity/ToolBox.java
+++ www-test-dao/src/main/java/com/testing/center/entity/ToolBox.java
@@ -1,47 +1,13 @@
 package com.testing.center.entity;
 
+import lombok.Data;
+
 import java.util.Objects;
 
+@Data
 public class ToolBox {
     private String tool_box_name;
     private Integer id;
     private String tool_box_colour;
 
-    public String getTool_box_colour() {
-        return tool_box_colour;
-    }
-
-    public void setTool_box_colour(String tool_box_colour) {
-        this.tool_box_colour = tool_box_colour;
-    }
-
-    public String getTool_box_name() {
-        return tool_box_name;
-    }
-
-    public void setTool_box_name(String tool_box_name) {
-        this.tool_box_name = tool_box_name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer tool_box_status) {
-        this.id = tool_box_status;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ToolBox toolBox = (ToolBox) o;
-        return Objects.equals(tool_box_name, toolBox.tool_box_name) &&
-                Objects.equals(id, toolBox.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(tool_box_name, id);
-    }
 }
