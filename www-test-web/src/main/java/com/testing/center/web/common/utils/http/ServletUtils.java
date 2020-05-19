892205dd720cfd1b7de1b606007c6b7a3460a09b
zhangly <zhanglylx@163.com>
2020-05-19 10:26:17 +0800

    新增ServletUtils.getUserIP

---
+++ www-test-web/src/main/java/com/testing/center/web/common/utils/http/ServletUtils.java
@@ -0,0 +1,53 @@
+package com.testing.center.web.common.utils.http;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ServletUtils {
+    /**
+     * @param request
+     * @return String
+     * @author HuaZai
+     * @contact who.seek.me@java98k.vip
+     * @title getUserIP
+     * <ul>
+     * @description 获取用户真实的IP地址
+     * </ul>
+     * @createdTime 2017年12月30日 下午6:42:17
+     * @version : V1.0.0
+     */
+    public static String getUserIP(HttpServletRequest request) {
+        String Xip = request.getHeader("X-Real-IP");
+        String XFor = request.getHeader("X-Forwarded-For");
+        if(StringUtils.isNotEmpty(XFor) && !"unKnown".equalsIgnoreCase(XFor)){
+            //多次反向代理后会有多个ip值，第一个ip才是真实ip
+            int index = XFor.indexOf(",");
+            if(index != -1){
+                return XFor.substring(0,index);
+            }else{
+                return XFor;
+            }
+        }
+        XFor = Xip;
+        if(StringUtils.isNotEmpty(XFor) && !"unKnown".equalsIgnoreCase(XFor)){
+            return XFor;
+        }
+        if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getHeader("Proxy-Client-IP");
+        }
+        if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getRemoteAddr();
+        }
+        return XFor;
+    }
+}
