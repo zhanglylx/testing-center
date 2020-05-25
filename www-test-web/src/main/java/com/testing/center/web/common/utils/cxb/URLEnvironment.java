7dca28ed843d76526f0e44db92c741aef142a96f
zhangly <zhanglylx@163.com>
2020-05-25 15:00:01 +0800

    加入webapp环境区分

--- www-test-web/src/main/java/com/testing/center/web/common/utils/cxb/URLEnvironment.java
+++ www-test-web/src/main/java/com/testing/center/web/common/utils/cxb/URLEnvironment.java
@@ -1,10 +1,21 @@
 package com.testing.center.web.common.utils.cxb;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.Properties;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class URLEnvironment {
     public static final int QA = 0;
     public static final int ONLINE = 1;
+    private static Properties properties;
+
+    @Value("#{URLEnvironment}")
+    public void setProperties(Properties properties) {
+        URLEnvironment.properties = properties;
+    }
 
     /**
      * 切换线上,默认为qa环境，如果switchOnline为qa，将不做任何转换直接返回
@@ -57,4 +68,22 @@ public class URLEnvironment {
                 throw new IllegalArgumentException("不支持的类型,：" + environment);
         }
     }
+
+    public static String contextSwitchingWebApp(String path, Integer environment) {
+        if (StringUtils.isBlank(path)) {
+            throw new NullPointerException("path不能为空");
+        }
+        if (environment == null) {
+            throw new NullPointerException("switchOnline不能为空");
+        }
+        switch (environment) {
+            case QA:
+                return properties.getProperty("cxb_webapp_qa_Host") + path;
+            case ONLINE:
+                return properties.getProperty("cxb_webapp_Online_Host") + path;
+            default:
+                throw new IllegalArgumentException("不支持的类型,：" + environment);
+        }
+    }
+
 }
